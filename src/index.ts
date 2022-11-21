4833d814f034ae3cc1620eac942980a4cd40a5d6
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-21 20:38:55 +0100

    Call start server function in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
+import "./loadEnvironments.js";
+import debugCreator from "debug";
+import chalk from "chalk";
+import { mongo } from "mongoose";
+import { port } from "./loadEnvironments.js";
+import startServer from "./server/startServer.js";
 
+const debug = debugCreator("users:root");
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const { MongoServerError } = mongo;
+
+try {
+  await startServer(+port);
+  debug(chalk.blue(`Server listening on port ${port}`));
+} catch (error: unknown) {
+  if (error instanceof MongoServerError) {
+    debug(
+      chalk.red(`Error connecting to the database ${(error as Error).message}`)
+    );
+  } else {
+    debug(chalk.red(`Error with the server ${(error as Error).message}`));
+  }
+}
