b2f648f4eab3ad301120b4b312b3319697d73c74
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-22 10:11:03 +0100

    Add exclusions to jest.config

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,4 +4,10 @@ module.exports = {
   testEnvironment: "node",
   testMatch: ["**/src/**/*.test.ts"],
   resolver: "jest-ts-webcompat-resolver",
+  testPathIgnorePatterns: [
+    "src/index.tsx",
+    "src/setupTests.ts",
+    "src/__snapshots__/*.snap",
+    "src/loadEnvironments.ts",
+  ],
 };
