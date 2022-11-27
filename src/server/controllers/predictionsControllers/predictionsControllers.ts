39efc552278997751d9f178095a14f8c89e0a9b0
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-27 17:24:56 +0100

    Add getPredictionById controller

--- src/server/controllers/predictionsControllers/predictionsControllers.ts
+++ src/server/controllers/predictionsControllers/predictionsControllers.ts
@@ -1,7 +1,7 @@
 import type { NextFunction, Response } from "express";
 import CustomError from "../../../CustomError/CustomError.js";
 import User from "../../../database/models/User.js";
-import type { CustomRequest } from "../../../types/types.js";
+import type { CustomRequest, PredictionData } from "../../../types/types.js";
 
 export const getPredictions = async (
   req: CustomRequest,
@@ -21,3 +21,64 @@ export const getPredictions = async (
     next(customError);
   }
 };
+
+export const getPredictionById = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { predictionId } = req.params;
+  const { userId } = req;
+
+  try {
+    const user = await User.findById(userId);
+
+    const prediction = user.predictions.find(
+      (prediction) => prediction._id.toString() === predictionId
+    );
+
+    if (!prediction) {
+      next(
+        new CustomError("Prediction not found", 404, "Prediction not found")
+      );
+
+      return;
+    }
+
+    res.status(200).json({ prediction });
+  } catch (error: unknown) {
+    const customError = new CustomError(
+      (error as Error).message,
+      400,
+      "Prediction not found"
+    );
+    next(customError);
+  }
+};
+
+// Export const createPrediction = async (
+//   req: CustomRequest,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   const { userId } = req;
+//   const { match, goalsTeam1, goalsTeam2 } = req.body as PredictionData;
+
+//   try {
+//     const user = await User.findById(userId);
+
+//     const prediction = {
+//       match,
+//       goalsTeam1,
+//       goalsTeam2,
+//     };
+
+//     user.predictions.push(prediction);
+
+//     await user.save();
+
+//     res.status(200).json({ prediction });
+//   } catch (error: unknown) {
+//     next(error);
+//   }
+// };
