69a04b96679d1faef93cfe4293cf80284d5d33bf
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-28 12:07:34 +0100

    Create Prediction Model

---
+++ src/database/models/Prediction.ts
@@ -0,0 +1,43 @@
+import { model, Schema } from "mongoose";
+import type { InferSchemaType } from "mongoose";
+
+const predictionSchema = new Schema({
+  match: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  goalsTeam1: {
+    type: Number,
+    required: true,
+  },
+  goalsTeam2: {
+    type: Number,
+    required: true,
+  },
+  redCards: {
+    type: Number,
+  },
+  yellowCards: {
+    type: Number,
+  },
+  penalties: {
+    type: Number,
+  },
+  picture: {
+    type: String,
+  },
+  backupPicure: {
+    type: String,
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+const Prediction = model("Prediction", predictionSchema, "predictions");
+
+export type PredictionStructure = InferSchemaType<typeof predictionSchema>;
+
+export default Prediction;
