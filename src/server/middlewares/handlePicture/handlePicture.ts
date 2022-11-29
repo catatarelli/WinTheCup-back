a789bf3a77b33a4a1437b1e7a9d770ba0fb2f147
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-29 16:29:10 +0100

    Add handlePicture middleware

---
+++ src/server/middlewares/handlePicture/handlePicture.ts
@@ -0,0 +1,58 @@
+import { createClient } from "@supabase/supabase-js";
+import path from "path";
+import fs from "fs/promises";
+import type { PredictionStructure } from "../../../database/models/Prediction.js";
+import {
+  supabaseUrl,
+  supabaseKey,
+  supabaseBucketId,
+} from "../../../loadEnvironments.js";
+import type { NextFunction, Response } from "express";
+import type { CustomRequest } from "../../../types/types.js";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+const bucket = supabase.storage.from(supabaseBucketId);
+
+const handlePicture = async (
+  req: CustomRequest<
+    Record<string, unknown>,
+    Record<string, unknown>,
+    PredictionStructure
+  >,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.file) {
+    next();
+    return;
+  }
+
+  const newFilePath = path.join(
+    "assets",
+    "images",
+    `${req.file.filename}${req.file.originalname}`
+  );
+
+  try {
+    await fs.rename(
+      path.join("assets", "images", req.file.filename),
+      newFilePath
+    );
+    const fileContent = await fs.readFile(newFilePath);
+
+    await bucket.upload(req.file.filename + req.file.originalname, fileContent);
+
+    const {
+      data: { publicUrl },
+    } = bucket.getPublicUrl(req.file.filename + req.file.originalname);
+
+    req.body.picture = newFilePath;
+    req.body.backupPicure = publicUrl;
+
+    next();
+  } catch (error: unknown) {
+    next(error);
+  }
+};
+
+export default handlePicture;
